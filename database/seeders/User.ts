f52ca2384e31b3147bda403149eb5b0f26dcaee1
Olakunle Boye <kunlexzy@gmail.com>
2022-10-15 21:00:42 +0100

    Added: middleware to kernel

--- database/seeders/User.ts
+++ database/seeders/User.ts
@@ -1,7 +1,17 @@
 import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import User from 'App/Models/User'
 
 export default class extends BaseSeeder {
   public async run () {
-    // Write your database queries inside the run method
+    await User.createMany([
+      {
+        email: 'virk@adonisjs.com',
+        password: 'secret',
+      },
+      {
+        email: 'romain@adonisjs.com',
+        password: 'supersecret'
+      }
+    ])
   }
 }
